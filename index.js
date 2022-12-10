84b2efe4ccb47f1ac199cc228c68c876662e9bf7
MaryKwawu <may419316@gmail.com>
2022-12-10 10:55:10 +0000

    added UI

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // A function that randomly returns rock, paper or scissors
-function Computerplay() {
+function computerPlay() {
   const randomNumber = Math.floor(Math.random() * 3);
 
   if (randomNumber === 0) return "rock";
@@ -11,14 +11,8 @@ function Computerplay() {
 function playRound(playerSelection, computerSelection) {
   const player = playerSelection.toLowerCase();
 
-  if (player === "rock" && computerSelection === "rock") {
-    return "Both chose rock. No winner";
-  }
-  if (player === "paper" && computerSelection === "paper") {
-    return "Both chose paper. No winner";
-  }
-  if (player === "scissors" && computerSelection === "scissors") {
-    return "Both chose scissors. No winner";
+  if (player === computerSelection) {
+    return "Both chose " + player + ". No winner";
   }
 
   // ROCK against SCISSORS
@@ -48,40 +42,111 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const computerSelection = Computerplay();
+let playerScore = 0;
+let computerScore = 0;
 
-// A function that keeps scores after 5 rounds of playing rock,paper scissors
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+window.onload = function () {
+  const rockbtn = document.getElementById("rockbtn");
+  const paperbtn = document.getElementById("paperbtn");
+  const scissorsbtn = document.getElementById("scissorsbtn");
 
-  for (let round = 0; round < 5; round++) {
-    //If not up to 5 rounds, play again
-    if (round < 5) {
-      const theWinner = playRound(
-        prompt("Enter rock, paper or scissors"),
-        computerSelection
-      );
-      console.log(theWinner);
+  rockbtn.onclick = function () {
+    const computerSelection = computerPlay();
+    const winner = playRound("rock", computerSelection);
+    trackScore(winner);
+    displayResults(winner);
+    displayScore();
 
-      if (theWinner.includes("You win")) {
-        playerScore = playerScore + 1;
-      }
-      if (theWinner.includes("You lose")) {
-        computerScore = computerScore + 1;
-      }
-    }
+    displayWinner();
+  };
 
-    // At the end of the round
-    if (round === 4) {
-      console.log("end of round");
-      if (playerScore > computerScore) {
-        alert("Player wins with " + playerScore + " points.");
-      } else {
-        alert("Computer wins with " + computerScore + " points.");
-      }
-    }
+  paperbtn.onclick = function () {
+    const computerSelection = computerPlay();
+    const winner = playRound("paper", computerSelection);
+    trackScore(winner);
+    displayResults(winner);
+    displayScore();
+
+    displayWinner();
+  };
+
+  scissorsbtn.onclick = function () {
+    const computerSelection = computerPlay();
+    const winner = playRound("scissors", computerSelection);
+    trackScore(winner);
+    displayResults(winner);
+    displayScore();
+
+    displayWinner();
+  };
+};
+
+function displayResults(results) {
+  document.getElementById("results").innerHTML = results;
+}
+
+function displayScore() {
+  document.getElementById("scores").innerHTML =
+    "Player Score: " + playerScore + " | " + "Computer score: " + computerScore;
+}
+
+function trackScore(winner) {
+  if (winner.includes("You win")) {
+    playerScore++;
+  } else if (winner.includes("You lose")) {
+    computerScore++;
   }
 }
 
-game();
+function resetScores() {
+  playerScore = 0;
+  computerScore = 0;
+  displayScore();
+}
+
+function displayWinner() {
+  if (playerScore == 5) {
+    document.getElementById("winner").innerHTML = "Player Wins!";
+
+    resetScores();
+  } else if (computerScore == 5) {
+    document.getElementById("winner").innerHTML = "Computer Wins!";
+    resetScores();
+  }
+}
+
+// A function that keeps scores after 5 rounds of playing rock,paper scissors
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   for (let round = 0; round < 5; round++) {
+//     //If not up to 5 rounds, play again
+//     if (round < 5) {
+//       const theWinner = playRound(
+//         prompt("Enter rock, paper or scissors"),
+//         computerSelection
+//       );
+//       console.log(theWinner);
+
+//       if (theWinner.includes("You win")) {
+//         playerScore = playerScore + 1;
+//       }
+//       if (theWinner.includes("You lose")) {
+//         computerScore = computerScore + 1;
+//       }
+//     }
+
+// At the end of the round
+//     if (round === 4) {
+//       console.log("end of round");
+//       if (playerScore > computerScore) {
+//         alert("Player wins with " + playerScore + " points.");
+//       } else {
+//         alert("Computer wins with " + computerScore + " points.");
+//       }
+//     }
+//   }
+// }
+
+// game();
